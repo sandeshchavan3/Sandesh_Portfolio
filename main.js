e0f058ea717388ab8f2d302b82ed1bb394660be8
sandesh3008 <chavansandesh112@gmail.com>
2025-07-02 00:42:33 +0530

    Slides added

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function toggleTheme(){
-    
+function toggleTheme() {
+    const htmlEl = document.documentElement;
+    const sun = document.getElementById("sun");
+    const moon = document.getElementById("moon");
+
+    htmlEl.classList.toggle("dark");
+    sun.classList.toggle("hide");
+    moon.classList.toggle("hide");
 }
