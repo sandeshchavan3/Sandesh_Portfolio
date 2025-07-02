5f924aabce20a32c13e929ffa9be8b050da41f6a
sandesh3008 <chavansandesh112@gmail.com>
2025-07-02 12:04:05 +0530

    links added

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const skillsDiv= document.getElementById("skills-div");
+const portDiv= document.getElementById("port-div")
+const skillsBtn= document.getElementById("skills-btn");
+const portBtn= document.getElementById("port-btn")
+
 function toggleTheme() {
     const htmlEl = document.documentElement;
     const sun = document.getElementById("sun");
@@ -6,4 +11,20 @@ function toggleTheme() {
     htmlEl.classList.toggle("dark");
     sun.classList.toggle("hide");
     moon.classList.toggle("hide");
+}
+
+function showPortfolio(){
+    skillsDiv.classList.add("hidden");
+    portDiv.classList.remove("hidden");
+
+    skillsBtn.style.backgroundColor("var(--bg-color)");
+    portBtn.style.backgroundColor("var(--theme-btn-bg)");
+}
+
+function showSkills(){
+    skillsDiv.classList.remove("hidden");
+    portDiv.classList.add("hidden");
+
+    skillsBtn.style.backgroundColor("var(--theme-btn-bg)");
+    portBtn.style.backgroundColor("var(--bg-color)");
 }
